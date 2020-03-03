1160d744235a0be68e89c2f2f96ca10f310fd7da
camthegeek <cam@camthegeek.net>
2020-03-02 19:29:17 -0500

    build on mac os; output as zip and .dmg; tip: requires mac os to build.

--- forge.config.js
+++ forge.config.js
@@ -31,6 +31,13 @@ module.exports = {
 	    productDescription: " Masari is a privacy-centric innovative cryptocurrency that allows users to send money around the globe with low fees very quickly.\n\n Masari ($MSR) is a proof of work peer-to-peer internet currency using the well researched and cryptographically sound CryptoNote and RingCT protocols. This means that every transaction origin, amount, and destination is obfuscated by default. \n\n Masari is an open source project that is fully decentralized, similar to bitcoin. With several CryptoNote firsts such as uncle mining via the SECOR protocol, a fully client side web wallet, and PoW sharding via the blocktree protocol, Masari is a fast, fungible, secure, private and soon-to-be scalable currency.",
 	    homepage: "https://getmasari.org/"
           }
+        },
+        {
+          name: "@electron-forger/maker-dmg",
+          config: {
+            name: "masari-gui-wallet",
+            icon: __dirname +'/src/assets/img/icons/favicon.icns',
+          }
         }
       ]
     }
